fe6076fa9a9807eabfa9e7587a881fcc94645f92
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2022-12-12 18:26:48 +0100

    Add ColumnForm component

--- react-app-project/src/components/Column/Column.js
+++ react-app-project/src/components/Column/Column.js
@@ -2,11 +2,11 @@ import styles from './Column.module.scss';
 
 const Column = props =>{
     return (
-        <artickle className={styles.column}>
+        <article className={styles.column}>
             <h2 className={styles.title}>
                 <span className={styles.icon + 'fa fa-' + props.icon} />
                 {props.title}</h2>
-        </artickle>
+        </article>
     );
 };
 
