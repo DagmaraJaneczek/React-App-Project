75aa7f1e00d21ae0c2a20d9361d8835bf1c99da5
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-04 15:53:32 +0100

    Add a serchForm action to reducer

--- react-app-project/src/components/SearchForm/SearchForm.js
+++ react-app-project/src/components/SearchForm/SearchForm.js
@@ -1,10 +1,19 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button'
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING' });
+    }
+
     return (
-        <form className={styles.searchForm}>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
             <TextInput placeholder="Search..." />
             <Button>
                 <span className="fa fa-search" />
