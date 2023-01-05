9a77593fde0fa367d41ee7274e1face2f95501f7
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-05 15:44:51 +0100

    Add new component NavBar

---
+++ react-app-project/src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () =>{
+    return(
+        <nav>
+            <div>
+                <a href='/'>
+                  <i class="fa fa-tasks" aria-hidden="true"></i>
+                </a>
+            </div>
+        </nav>
+
+    )
+}
+
+export default NavBar;
