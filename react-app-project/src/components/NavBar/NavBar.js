cea8e12c036ae204f6d0ccd2f7f18ae102642b5b
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-07 18:21:45 +0100

    Correction to Navbar - Change a to NavLink

--- react-app-project/src/components/NavBar/NavBar.js
+++ react-app-project/src/components/NavBar/NavBar.js
@@ -1,18 +1,19 @@
 import styles from './NavBar.module.scss'
 import Container from '../Container/Container'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () =>{
     return(
         <nav className={styles.navbar}>
           <Container>
           <div className={styles.nav_container}>
-                <a className={styles.nav_icon} href='/'>
+                <a href='/' className={styles.nav_icon}>
                   <i className='fa fa-tasks'></i>
                 </a>
                 <ul className={styles.nav_links}>
-                  <li><a href='/'>Home</a></li>
-                  <li><a href='/'>Favorite</a></li>
-                  <li><a href='/'>About</a></li>
+                  <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined } to ='/'>Home</NavLink></li>
+                  <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined } to ='/favorite'>Favorite</NavLink></li>
+                  <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined } to ='/about'>About</NavLink></li>
                 </ul>
             </div>
           </Container>
