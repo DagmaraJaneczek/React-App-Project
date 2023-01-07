935f9751ebb989de8606f0b21af58420f30d4319
DagmaraJaneczek <janeczek.dagmara@gmail.com>
2023-01-07 16:30:16 +0100

    Add Navbar

--- react-app-project/src/components/NavBar/NavBar.js
+++ react-app-project/src/components/NavBar/NavBar.js
@@ -1,11 +1,22 @@
+import styles from './NavBar.module.scss'
+import Container from '../Container/Container'
+
 const NavBar = () =>{
     return(
-        <nav>
-            <div>
-                <a href='/'>
-                  <i class="fa fa-tasks" aria-hidden="true"></i>
+        <nav className={styles.navbar}>
+          <Container>
+          <div className={styles.nav_container}>
+                <a className={styles.nav_icon} href='/'>
+                  <i className='fa fa-tasks'></i>
                 </a>
+                <ul className={styles.nav_links}>
+                  <li><a href='/'>Home</a></li>
+                  <li><a href='/'>Favorite</a></li>
+                  <li><a href='/'>About</a></li>
+                </ul>
             </div>
+          </Container>
+
         </nav>
 
     )
